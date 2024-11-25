60b244c253ce9574dc93691b60453439ccea9634
FabCode67 <mwanafunzifabrice@gmail.com>
2024-11-25 22:23:20 +0200

    quick fix

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://komparas.netlify.app/",
+    origin: "https://komparas.netlify.app",
     methods: ["GET", "POST"]
   }
 });
@@ -30,7 +30,7 @@ io.on('connection', (socket) => {
 });
 
 const corsOpts = {
-  origin: "https://komparas.netlify.app/", 
+  origin: "https://komparas.netlify.app", 
   methods: ['GET', 'POST', 'DELETE', 'PATCH', 'PUT'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
